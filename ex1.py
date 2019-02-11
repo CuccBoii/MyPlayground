98299228e140037e084060cfe09ba79177d070ee
Guy Eden <guyawe2011@gmail.com>
2019-02-11 13:07:45 +0200

    Added main menu and main loop

--- ex1.py
+++ ex1.py
@@ -1,6 +1,10 @@
+import os
+
 secret_code = 8282
 balance = 80.00
 authorized = False
+options = "Please select an option:\n1.View Account Balance.\n2.Withdraw money.\n3.Change secret code.\n4.Exit.\n"
+withdraw_options = "Please select an option:\n1.20 nis.\n2.50 nis.\n3.Other.\n"
 
 # Asks for user input.
 # Returns true if the code the user's
@@ -8,11 +12,11 @@ authorized = False
 def auth():
     print("Input secret code:")
     input_code = input()
-    if input_code == secret_code:
+    if input_code == str(secret_code):
         print("Authorization Succesful\n")
     else:
         print("Authorization Failed\n")
-    return input_code == secret_code
+    return input_code == str(secret_code)
 
 
 def print_balance():
@@ -26,12 +30,34 @@ def withdraw(amount):
     else:
         balance = balance - amount
 
+def withdraw_menu():
+    cls()
+
+def main_menu(keep_running):
+    while(keep_running):
+        print(options)
+        op = int(input())
+        if op == 1: 
+            print_balance()
+        elif op == 2: 
+            withdraw_menu()
+        elif op == 3: 
+            print("Please input the new secret code:")
+            secret_code = input()
+        elif op == 4: 
+            keep_running = False
+        else:
+            print("Invalid input.\nTry again:")
+
+
+
 
 def main():
+    global authorized
     while authorized is False:
-        auth()
-    
-    print_balance()
+        authorized = auth()
+    os.system('clear')
+    main_menu(True)
 
 main()
 
