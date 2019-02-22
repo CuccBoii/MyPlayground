c6beb792ef5b50af63adcf947b704c533c8907a5
Guy-Eden <guyawe2011@gmail.com>
2019-02-22 13:57:32 +0200

    Added two checks, one checks the length of the input and the other checks if its only digits.

--- ex5.py
+++ ex5.py
@@ -3,6 +3,6 @@ from functools import reduce
 # gets an id number as input, prints if its valid or not.
 def is_id_valid():
     string = input("Enter id number:\n")
-    print("valid") if (sum(list(map(lambda x, y: int(x) + (int(y)*2 if int(y)*2 <= 9 else int(y)*2 - 9) ,string[0:7:2], string[1:8:2])))+int(string[8])) % 10 == 0 else print("Not valid")
+    print("valid") if len(string) == 9 and string.isdigit() and (sum(list(map(lambda x, y: int(x) + (int(y)*2 if int(y)*2 <= 9 else int(y)*2 - 9) ,string[0:7:2], string[1:8:2])))+int(string[8])) % 10 == 0 else print("Not valid")
 
 is_id_valid()
